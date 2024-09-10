abd47a84a5bfd6c77938455d5ce8eae77bc8be44
Nicolas Ho <nicolas.ho.edu@gmail.com>
2024-09-10 10:36:09 +0200

    Ajout d'une nouvelle classe

--- apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Democracy"},{"p":"<Unnamed>","l":"Impro"},{"p":"<Unnamed>","l":"RandomSentence"}];updateSearchResults();
