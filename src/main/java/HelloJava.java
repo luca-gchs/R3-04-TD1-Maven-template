da694da8ab441216ed967e6e39331181d20c8e54
luca-gchs <lucagaches2@gmail.com>
2024-09-10 09:17:35 +0200

    commit de Luca TP1

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -4,7 +4,12 @@
  * @author LC
  */
 class HelloJava {
-	public afficherCancan(){
 
+    public static void main(String[] args) {
+		System.out.println("Hello Blagnac");
+	}
+
+	public void afficherCancan(){
 	}
 }
+
